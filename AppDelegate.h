a4a00883a02bbfc1b3deb461a26f00960d318120
liuyunda <liuyunda@meituan.com>
2017-11-06 16:58:52 +0800

    测试提交appDelegate

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  webview_ios
+//
+//  Created by liuyunda on 17/1/20.
+//  Copyright © 2017年 liuyunda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
